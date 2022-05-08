1b637a7f7357ff89867bbd1af0164e885e52b0e3
Essential Randomness <essential.randomn3ss@gmail.com>
2022-05-08 14:55:23 -0700

    extracted profile card in its own component

--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -1,25 +1,14 @@
-import avatar from "public/avatar.png";
+import {
+  ProfileCard,
+  links as ProfileCardsLinks,
+} from "../../components/ProfileCard";
+
 import styles from "./index.css";
 
 export default function Index() {
   return (
     <div style={{ fontFamily: "system-ui, sans-serif", lineHeight: "1.4" }}>
-      <section className="h-card">
-        <img className="avatar u-logo" src={avatar} alt="Ms Boba's avatar" />
-        <h1 className="p-name">
-          <a className="u-url" href="/">
-            Essential Randomness
-          </a>
-        </h1>
-        <div>
-          a.k.a. <span className="p-nickname">Ms Boba</span>
-        </div>
-        <div className="p-note">
-          Software Engineer, Fandom Person & Other Random Stuff. Big mad at big
-          tech. Full Stack Human. #queeredge — Building chaotic ethical social
-          spaces at <a href="https://bobaboard.com/">www.bobaboard.com</a>!
-        </div>
-      </section>
+      <ProfileCard />
       <section>
         <h2>Where to find me on the web</h2>
         <div className="social-links">
@@ -32,5 +21,6 @@ export default function Index() {
 }
 
 export function links() {
-  return [{ rel: "stylesheet", href: styles }];
+  console.log(ProfileCardsLinks());
+  return [{ rel: "stylesheet", href: styles }, ...ProfileCardsLinks()];
 }
---
+++ components/ProfileCard.tsx
@@ -0,0 +1,27 @@
+import avatar from "public/avatar.png";
+import styles from "./ProfileCard.css";
+
+export const ProfileCard = () => {
+  return (
+    <section className="h-card">
+      <img className="avatar u-logo" src={avatar} alt="Ms Boba's avatar" />
+      <h1 className="p-name">
+        <a className="u-url" href="/">
+          Essential Randomness
+        </a>
+      </h1>
+      <div>
+        a.k.a. <span className="p-nickname">Ms Boba</span>
+      </div>
+      <div className="p-note">
+        Software Engineer, Fandom Person & Other Random Stuff. Big mad at big
+        tech. Full Stack Human. #queeredge — Building chaotic ethical social
+        spaces at <a href="https://bobaboard.com/">www.bobaboard.com</a>!
+      </div>
+    </section>
+  );
+};
+
+export function links() {
+  return [{ rel: "stylesheet", href: styles }];
+}
