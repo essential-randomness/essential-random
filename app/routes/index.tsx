b848cfe90d752cfc1b7e6c616c104b78c39470b4
Essential Randomness <essential.randomn3ss@gmail.com>
2022-05-08 14:39:06 -0700

    Added h-card

--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -4,16 +4,22 @@ import styles from "./index.css";
 export default function Index() {
   return (
     <div style={{ fontFamily: "system-ui, sans-serif", lineHeight: "1.4" }}>
-      <img className="avatar" src={avatar} alt="Ms Boba's avatar" />
-      <div>
-        <h1>Essential Randomness</h1>
-        <div>a.k.a. Ms Boba</div>
-      </div>
-      <div>
-        Software Engineer, Fandom Person & Other Random Stuff. Big mad at big
-        tech. Full Stack Human. #queeredge — Building chaotic ethical social
-        spaces at <a href="https://bobaboard.com/">www.bobaboard.com</a>!
-      </div>
+      <section className="h-card">
+        <img className="avatar u-logo" src={avatar} alt="Ms Boba's avatar" />
+        <h1 className="p-name">
+          <a className="u-url" href="/">
+            Essential Randomness
+          </a>
+        </h1>
+        <div>
+          a.k.a. <span className="p-nickname">Ms Boba</span>
+        </div>
+        <div className="p-note">
+          Software Engineer, Fandom Person & Other Random Stuff. Big mad at big
+          tech. Full Stack Human. #queeredge — Building chaotic ethical social
+          spaces at <a href="https://bobaboard.com/">www.bobaboard.com</a>!
+        </div>
+      </section>
       <section>
         <h2>Where to find me on the web</h2>
         <div className="social-links">
