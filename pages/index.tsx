022bc5dd30385015f0628bcc0082f4aa995fd1d2
Essential Randomness <essential.randomn3ss@gmail.com>
2022-08-15 16:49:13 -0700

    remove imports

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,8 +1,7 @@
 import Head from "next/head";
-import Image from "next/image";
+import Link from "next/link";
 import type { NextPage } from "next";
 import { ProfileCard } from "../components/ProfileCard";
-import styles from "../styles/Home.module.css";
 
 const Home: NextPage = () => {
   return (
