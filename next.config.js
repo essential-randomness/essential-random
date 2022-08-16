14ebf05d9669aca5a692a1e6171e1f8495b1344d
Essential Randomness <essential.randomn3ss@gmail.com>
2022-08-15 17:22:34 -0700

    allow loading mdx pages as is

--- next.config.js
+++ next.config.js
@@ -1,25 +1,13 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-  swcMinify: true,
-  webpack: (
-    config,
-    { buildId, dev, isServer, defaultLoaders, nextRuntime, webpack }
-  ) => {
-    config.module.rules.push({
-      test: /\.mdx?$/,
-      use: [
-        {
-          loader: "@mdx-js/loader",
-          /** @type {import('@mdx-js/loader').Options} */
-          options: {
-            /* jsxImportSource: …, otherOptions… */
-          },
-        },
-      ],
-    });
-    return config;
+const withMDX = require("@next/mdx")({
+  extension: /\.mdx?$/,
+  options: {
+    remarkPlugins: [],
+    rehypePlugins: [],
+    // If you use `MDXProvider`, uncomment the following line.
+    // providerImportSource: "@mdx-js/react",
   },
-};
-
-module.exports = nextConfig;
+});
+module.exports = withMDX({
+  // Append the default value with md extensions
+  pageExtensions: ["ts", "tsx", "js", "jsx", "md", "mdx"],
+});
