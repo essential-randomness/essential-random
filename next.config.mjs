65d639705f433864e9ef02af16ec17c6a54e96f0
Essential Randomness <essential.randomn3ss@gmail.com>
2022-08-15 20:47:17 -0700

    parse frontmatter with plugin

--- next.config.mjs
+++ next.config.mjs
@@ -1,13 +1,17 @@
-const withMDX = require("@next/mdx")({
+import nextMdx from "@next/mdx";
+import remarkFrontmatter from "remark-frontmatter";
+const withMDX = nextMdx({
   extension: /\.mdx?$/,
   options: {
-    remarkPlugins: [],
+    // Consider swapping with https://github.com/jonschlinkert/gray-matter
+    remarkPlugins: [remarkFrontmatter],
     rehypePlugins: [],
     // If you use `MDXProvider`, uncomment the following line.
     // providerImportSource: "@mdx-js/react",
   },
 });
-module.exports = withMDX({
+
+export default withMDX({
   // Append the default value with md extensions
   pageExtensions: ["ts", "tsx", "js", "jsx", "md", "mdx"],
 });
