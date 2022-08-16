28cf6ab3cfd1a6b2ae04c869963e221d73286f9c
Essential Randomness <essential.randomn3ss@gmail.com>
2022-08-15 20:56:11 -0700

    allow using frontmatter variables in posts

--- next.config.mjs
+++ next.config.mjs
@@ -1,10 +1,13 @@
 import nextMdx from "@next/mdx";
 import remarkFrontmatter from "remark-frontmatter";
+import remarkMdxFrontmatter from "remark-mdx-frontmatter";
+
+const REMARK_PLUGINS = [remarkFrontmatter, remarkMdxFrontmatter];
 const withMDX = nextMdx({
   extension: /\.mdx?$/,
   options: {
     // Consider swapping with https://github.com/jonschlinkert/gray-matter
-    remarkPlugins: [remarkFrontmatter],
+    remarkPlugins: REMARK_PLUGINS,
     rehypePlugins: [],
     // If you use `MDXProvider`, uncomment the following line.
     // providerImportSource: "@mdx-js/react",
@@ -15,6 +18,6 @@ export default withMDX({
   // Append the default value with md extensions
   pageExtensions: ["ts", "tsx", "js", "jsx", "md", "mdx"],
   serverRuntimeConfig: {
-    remarkPlugins: [remarkFrontmatter],
+    remarkPlugins: REMARK_PLUGINS,
   },
 });
