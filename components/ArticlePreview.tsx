ff94378386078f410e123aa4864d16363a6eba9d
Essential Randomness <essential.randomn3ss@gmail.com>
2022-08-20 16:45:39 -0700

    Add preview for blog articles

---
+++ components/ArticlePreview.tsx
@@ -0,0 +1,38 @@
+import Image from "next/future/image";
+import Link from "next/link";
+import avatar from "../public/avatar.png";
+import path from "path";
+
+export interface ArticlePreviewProps {
+  url: string;
+  title: string;
+  summary: string;
+  createdAt: Date;
+}
+
+export const ArticlePreview = (props: ArticlePreviewProps) => {
+  return (
+    <article key={props.url}>
+      <header>
+        <Image className="avatar u-logo" src={avatar} alt="Ms Boba's avatar" />
+        <h2>
+          <Link href={props.url}>
+            <a>{props.title}</a>
+          </Link>
+        </h2>
+        <time dateTime={String(props.createdAt)}>
+          {props.createdAt.toLocaleDateString()}
+        </time>
+      </header>
+      <div className="content">{props.summary}</div>
+      <footer>
+        <div>3 replies</div>
+        <ul>
+          <li>like</li>
+          <li>copy url</li>
+          <li>comment</li>
+        </ul>
+      </footer>
+    </article>
+  );
+};
