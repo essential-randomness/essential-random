4e05ee882dcfd5dc569bff0faaf281b048364480
Essential Randomness <essential.randomn3ss@gmail.com>
2022-08-20 18:47:43 -0700

    Add MDX utils to manipulate blog post title

---
+++ utils/mdx-utils.ts
@@ -0,0 +1,20 @@
+import { remove } from "unist-util-remove";
+import { select } from "hast-util-select";
+import { toText } from "hast-util-to-text";
+
+export const rehypeExtractTitle = () => {
+  return (tree: Parameters<typeof select>[1]) => {
+    const h1 = select("h1", tree);
+    if (!h1) {
+      return null;
+    }
+    // Create a single text element whose value is the h1 content
+    return { type: "text", value: toText(h1) };
+  };
+};
+
+export const rehypeRemoveTitle = () => {
+  return (tree: Parameters<typeof remove>[0]) => {
+    remove(tree, { type: "element", tagName: "h1" });
+  };
+};
