9f70905366b35c5346d8404d21d40d8c3a879890
Essential Randomness <essential.randomn3ss@gmail.com>
2022-11-06 14:30:25 -0800

    get contentlayer to actually work

--- contentlayer.config.ts
+++ contentlayer.config.ts
@@ -2,7 +2,7 @@ import { defineDocumentType, makeSource } from "contentlayer/source-files";
 
 export const Post = defineDocumentType(() => ({
   name: "Post",
-  filePathPattern: `**/*.mdx`,
+  filePathPattern: `blog/**/*.mdx`,
   contentType: "mdx",
   fields: {
     title: {
@@ -19,12 +19,41 @@ export const Post = defineDocumentType(() => ({
   computedFields: {
     url: {
       type: "string",
-      resolve: (post) => `/posts/${post._raw.flattenedPath}`,
+      resolve: (post) =>
+        `/content/${post._raw.flattenedPath.substring("blog/".length)}`,
+    },
+  },
+}));
+
+export const Project = defineDocumentType(() => ({
+  name: "Project",
+  filePathPattern: `projects/**/*.mdx`,
+  contentType: "mdx",
+  fields: {
+    name: {
+      type: "string",
+      description: "The name of the project",
+      required: true,
+    },
+    links: {
+      type: "json",
+      description: "The links to the project",
+      required: true,
+    },
+  },
+  computedFields: {
+    url: {
+      type: "string",
+      resolve: (project) => `/projects/${project._raw.flattenedPath}`,
+    },
+    title: {
+      type: "string",
+      resolve: (post) => console.log(Object.keys(post.body.raw)),
     },
   },
 }));
 
 export default makeSource({
-  contentDirPath: "content/blog/",
-  documentTypes: [Post],
+  contentDirPath: "content/",
+  documentTypes: [Post, Project],
 });
