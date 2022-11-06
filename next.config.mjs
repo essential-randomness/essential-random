4005dec6631edd8894663b4cc6231e96c5ee32de
Essential Randomness <essential.randomn3ss@gmail.com>
2022-11-06 13:52:59 -0800

    get posts to work

--- next.config.mjs
+++ next.config.mjs
@@ -1,29 +1,27 @@
 import nextMdx from "@next/mdx";
 import remarkFrontmatter from "remark-frontmatter";
 import remarkMdxFrontmatter from "remark-mdx-frontmatter";
+import { withContentlayer } from "next-contentlayer";
 
 export const REMARK_PLUGINS = [remarkFrontmatter, remarkMdxFrontmatter];
-const withMDX = nextMdx({
-  extension: /\.mdx?$/,
-  options: {
-    // TODO: Consider swapping with https://github.com/jonschlinkert/gray-matter
-    remarkPlugins: REMARK_PLUGINS,
-    rehypePlugins: [],
-    // If you use `MDXProvider`, uncomment the following line.
-    // providerImportSource: "@mdx-js/react",
-  },
-});
+// const withMDX = nextMdx({
+//   extension: /\.mdx?$/,
+//   options: {
+//     // TODO: Consider swapping with https://github.com/jonschlinkert/gray-matter
+//     remarkPlugins: REMARK_PLUGINS,
+//     rehypePlugins: [],
+//     // If you use `MDXProvider`, uncomment the following line.
+//     // providerImportSource: "@mdx-js/react",
+//   },
+// });
 
-export default withMDX({
+export default withContentlayer({
   // Append the default value with md extensions
   pageExtensions: ["ts", "tsx", "js", "jsx", "md", "mdx"],
   serverRuntimeConfig: {
     remarkPlugins: REMARK_PLUGINS,
   },
   experimental: {
-    images: {
-      allowFutureImage: true,
-    },
     scrollRestoration: true,
   },
 });
