c0e85bc517897fbec3eac1deed667e505ae9cdcf
Essential Randomness <essential.randomn3ss@gmail.com>
2022-11-06 14:46:36 -0800

    remove useless configs

--- next.config.mjs
+++ next.config.mjs
@@ -1,26 +1,6 @@
-import nextMdx from "@next/mdx";
-import remarkFrontmatter from "remark-frontmatter";
-import remarkMdxFrontmatter from "remark-mdx-frontmatter";
 import { withContentlayer } from "next-contentlayer";
 
-export const REMARK_PLUGINS = [remarkFrontmatter, remarkMdxFrontmatter];
-// const withMDX = nextMdx({
-//   extension: /\.mdx?$/,
-//   options: {
-//     // TODO: Consider swapping with https://github.com/jonschlinkert/gray-matter
-//     remarkPlugins: REMARK_PLUGINS,
-//     rehypePlugins: [],
-//     // If you use `MDXProvider`, uncomment the following line.
-//     // providerImportSource: "@mdx-js/react",
-//   },
-// });
-
 export default withContentlayer({
-  // Append the default value with md extensions
-  pageExtensions: ["ts", "tsx", "js", "jsx", "md", "mdx"],
-  serverRuntimeConfig: {
-    remarkPlugins: REMARK_PLUGINS,
-  },
   experimental: {
     scrollRestoration: true,
   },
