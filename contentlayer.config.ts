a483936847a57010457367de61c5e6f23e8128d4
Essential Randomness <essential.randomn3ss@gmail.com>
2022-11-06 13:52:59 -0800

    get posts to work

---
+++ contentlayer.config.ts
@@ -0,0 +1,30 @@
+import { defineDocumentType, makeSource } from "contentlayer/source-files";
+
+export const Post = defineDocumentType(() => ({
+  name: "Post",
+  filePathPattern: `**/*.mdx`,
+  contentType: "mdx",
+  fields: {
+    title: {
+      type: "string",
+      description: "The title of the post",
+      required: true,
+    },
+    created_at: {
+      type: "date",
+      description: "The date of the post",
+      required: true,
+    },
+  },
+  computedFields: {
+    url: {
+      type: "string",
+      resolve: (post) => `/posts/${post._raw.flattenedPath}`,
+    },
+  },
+}));
+
+export default makeSource({
+  contentDirPath: "content/blog/",
+  documentTypes: [Post],
+});
