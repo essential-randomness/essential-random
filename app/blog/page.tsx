8abac95dee47e5945707b03a5831cffd0948878f
Essential Randomness <essential.randomn3ss@gmail.com>
2022-11-06 12:54:06 -0800

    more not working nextjs 13

--- app/blog/page.tsx
+++ app/blog/page.tsx
@@ -1,21 +1,18 @@
 import * as runtime from "react/jsx-runtime";
 
 import type { GetStaticProps, NextPage } from "next";
-import {
-  getBlogEntriesInContentPath,
-  getStaticBlogPages,
-} from "../../utils/path-utils";
 
 import { ArticlePreview } from "../../components/ArticlePreview";
 import { Nav } from "../../components/Nav";
 import { ProfileCard } from "../../components/ProfileCard";
 import { ProfileNav } from "../../components/ProfileNav";
+import { getBlogEntriesInContentPath } from "../../utils/path-utils";
 import { runSync } from "@mdx-js/mdx";
 
 const getAllBlogEntries = async () => {
-  const [blogEntries, blogPages] = await Promise.all([
+  const [blogEntries] = await Promise.all([
     getBlogEntriesInContentPath(),
-    getStaticBlogPages(),
+    // getStaticBlogPages(),
   ]);
 
   const finalBlogEntries = new Map<
@@ -25,37 +22,41 @@ const getAllBlogEntries = async () => {
   // Blog pages have higher priority than blog entries. We add them last so
   // they'll override any blog entry with the same name.
   blogEntries.forEach((entry) => finalBlogEntries.set(entry.slug, entry));
-  blogPages.forEach((entry) => finalBlogEntries.set(entry.slug, entry));
+  // blogPages.forEach((entry) => finalBlogEntries.set(entry.slug, entry));
 
   return [...finalBlogEntries.values()].sort(
     (a, b) => b.createdAt.getTime() - a.createdAt.getTime()
   );
 };
 
-export const getStaticProps: GetStaticProps = async ({ params }) => {
-  return {
-    props: {
-      // TODO: we cannot serialize this with date
-      paths: (await getAllBlogEntries()).map((entry) => ({
-        ...entry,
-        createdAt: entry.createdAt.toISOString(),
-      })) as Array<
-        Omit<Awaited<ReturnType<typeof getAllBlogEntries>>[0], "createdAt"> & {
-          createdAt: string;
-        }
-      >,
-    },
-  };
-};
+// export const getStaticProps: GetStaticProps = async ({ params }) => {
+//   return {
+//     props: {
+//       // TODO: we cannot serialize this with date
+//       paths: (await getAllBlogEntries()).map((entry) => ({
+//         ...entry,
+//         createdAt: entry.createdAt.toISOString(),
+//       })) as Array<
+//         Omit<Awaited<ReturnType<typeof getAllBlogEntries>>[0], "createdAt"> & {
+//           createdAt: string;
+//         }
+//       >,
+//     },
+//   };
+// };
 
-export default function Page({
-  paths,
-}: {
-  paths: Awaited<ReturnType<typeof getAllBlogEntries>>;
-}) {
+export default async function Page() {
+  const paths = (await getAllBlogEntries()).map((entry) => ({
+    ...entry,
+    createdAt: entry.createdAt.toISOString(),
+  })) as Array<
+    Omit<Awaited<ReturnType<typeof getAllBlogEntries>>[0], "createdAt"> & {
+      createdAt: string;
+    }
+  >;
   return (
     <ol>
-      {[paths].map((path) => {
+      {paths.map((path) => {
         return (
           <li key={path.slug}>
             <ArticlePreview
