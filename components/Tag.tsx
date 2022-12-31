f45682e955f5b575e73c91d5d2dba4a45a67193c
Essential Randomness <essential.randomn3ss@gmail.com>
2022-12-31 01:25:04 -0800

    prettify tags

--- components/Tag.tsx
+++ components/Tag.tsx
@@ -1,6 +1,11 @@
 export const Tag = ({ tag }: { tag: string }) => {
   // TODO: find a better way to split
-  const [category, name] = tag.split(/^([^:]+):/gm).filter((s) => s.length > 0);
+  const split = tag.split(/^([^:]+):/gm).filter((s) => s.length > 0);
+  if (split.length == 1) {
+    split.unshift("");
+  }
+
+  const [category, name] = split;
   return (
     <div className="tag" data-category={category}>
       {category && <div className="category">{category}</div>}
