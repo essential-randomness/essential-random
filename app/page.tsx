ddc972f7cc4523267293ef438c6d88da804d1754
Essential Randomness <essential.randomn3ss@gmail.com>
2023-01-02 16:57:49 -0800

    improve nav layout

--- app/page.tsx
+++ app/page.tsx
@@ -5,12 +5,12 @@ import socials from "../content/_socials.yml";
 
 export default function Page() {
   return (
-    <>
+    <main className="home">
       <ProfileCard />
       <section>
         <h2>As seen on</h2>
         <SocialLinks links={socials} />
       </section>
-    </>
+    </main>
   );
 }
--- components/Nav.tsx
+++ components/Nav.tsx
@@ -1,20 +1,25 @@
+"use client";
+
 import Link from "next/link";
+import cn from "classnames";
+import { useSelectedLayoutSegment } from "next/navigation";
 
 export const Nav = () => {
+  const currentSection = useSelectedLayoutSegment();
   return (
     <header>
       <nav>
         <ul data-current={true}>
-          <li>
+          <li className={cn({ current: currentSection == null })}>
             <Link href={"/"}>Home</Link>
           </li>
-          <li className="current">
+          <li className={cn({ current: currentSection == "posts" })}>
             <Link href={"/posts"}>Blog</Link>
           </li>
-          <li>
+          <li className={cn({ current: currentSection == "projects" })}>
             <Link href={"/projects"}>Projects</Link>
           </li>
-          <li>
+          <li className={cn({ current: currentSection == "support-me" })}>
             <Link href={"/support-me"}>Support me</Link>
           </li>
         </ul>
