017bde5a88915bff5e75dfc8c7d0c39c217bbbac
Essential Randomness <essential.randomn3ss@gmail.com>
2023-08-13 23:42:12 -0700

    create global layout

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -1,26 +1,21 @@
-"use client";
+import { clsx } from 'clsx';
 
-import Link from "next/link";
-import cn from "classnames";
-import { useSelectedLayoutSegment } from "next/navigation";
-
-export const Nav = () => {
-  const currentSection = useSelectedLayoutSegment();
+export const Nav = ({currentPath}: {currentPath: string}) => {
   return (
     <header>
       <nav>
         <ul data-current={true}>
-          <li className={cn({ current: currentSection == null })}>
-            <Link href={"/"}>Home</Link>
+          <li className={clsx({ current: currentPath == "/" })}>
+            <a href={"/"}>Home</a>
           </li>
-          <li className={cn({ current: currentSection == "posts" })}>
-            <Link href={"/posts"}>Blog</Link>
+          <li className={clsx({ current: currentPath == "/posts" })}>
+            <a href={"/posts"}>Blog</a>
           </li>
-          <li className={cn({ current: currentSection == "projects" })}>
-            <Link href={"/projects"}>Projects</Link>
+          <li className={clsx({ current: currentPath == "/projects" })}>
+            <a href={"/projects"}>Projects</a>
           </li>
-          <li className={cn({ current: currentSection == "support-me" })}>
-            <Link href={"/support-me"}>Support me</Link>
+          <li className={clsx({ current: currentPath == "/support-me" })}>
+            <a href={"/support-me"}>Support me</a>
           </li>
         </ul>
       </nav>
