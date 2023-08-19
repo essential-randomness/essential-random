cf6c22da17e3e5f7c2b54be81da6cf725aa22b31
Essential Randomness <essential.randomn3ss@gmail.com>
2023-08-19 09:20:48 -0700

    finish support me page

--- src/components/TiersDisplay.tsx
+++ src/components/TiersDisplay.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import React from "react";
-import { SupportTierCard } from "components/SupportTierCard";
+import { SupportTierCard } from "./SupportTierCard";
 
 export type TierType = {
   name: string;
