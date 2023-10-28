d847be4cb169fb27e70f5836d244e07cd571b5b4
Essential Randomness <essential.randomn3ss@gmail.com>
2023-10-28 16:45:39 -0700

    autolink headings in mdx files

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,14 @@
+/**
+ * @typedef {import('hast').ElementContent} ElementContent
+ */
+
 import { defineConfig } from "astro/config";
+import { fromHtmlIsomorphic } from "hast-util-from-html-isomorphic";
 import mdx from "@astrojs/mdx";
 import react from "@astrojs/react";
+import rehypeAutolinkHeadings from "rehype-autolink-headings";
 import rehypeCustomEmoji from "rehype-custom-emoji";
+import rehypeSlug from "rehype-slug";
 import rehypeToc from "rehype-toc";
 
 // https://astro.build/config
@@ -11,6 +18,13 @@ export default defineConfig({
   markdown: {
     remarkPlugins: [],
     rehypePlugins: [
+      rehypeSlug,
+      [
+        rehypeAutolinkHeadings,
+        {
+          behavior: "wrap",
+        },
+      ],
       rehypeToc,
       [
         rehypeCustomEmoji,
