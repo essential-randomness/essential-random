57b12ea70c0d910d64473bf8e2160e22fa56a596
Essential Randomness <essential.randomn3ss@gmail.com>
2024-01-09 00:45:48 -0800

    fix styling of fujoverse page

--- src/content/posts/_config.ts
+++ src/content/posts/_config.ts
@@ -1,10 +1,11 @@
 import { defineCollection, z } from "astro:content";
+import { parseInline } from "marked";
 
 export const posts = defineCollection({
   type: "content",
   schema: ({ image }) =>
     z.object({
-      title: z.string(),
+      title: z.string().transform((title) => parseInline(title)),
       tagline: z.string().optional(),
       created_at: z.date(),
       unlisted: z.boolean().optional().default(false),
