e412646384de5763966a8aa122a328dc209485e7
Essential Randomness <essential.randomn3ss@gmail.com>
2024-02-08 15:34:56 -0800

    get current stream to show up in schedule

--- src/components/streams/utils.ts
+++ src/components/streams/utils.ts
@@ -1,3 +1,5 @@
+const HOUR_MS = 60 * 60 * 1000;
+
 export const filterStreamsByTime =
   ({ after, before }: { after: Date; before: Date }) =>
   (stream: { data: { scheduled_at: Date } }) =>
@@ -13,17 +15,35 @@ export const findNextStream = (
     const streamDate = new Date(
       "data" in stream ? stream.data.scheduled_at : stream.scheduled_at
     );
+    console.log(stream);
+    console.log(isCurrentStream(stream));
     if (
-      streamDate > new Date(Date.now()) &&
-      (nextStream === null ||
-        new Date(
-          "data" in nextStream
-            ? nextStream.data.scheduled_at
-            : nextStream.scheduled_at
-        ) > streamDate)
+      isCurrentStream(stream) ||
+      (streamDate > new Date(Date.now()) &&
+        (nextStream === null ||
+          new Date(
+            "data" in nextStream
+              ? nextStream.data.scheduled_at
+              : nextStream.scheduled_at
+          ) > streamDate))
     ) {
       nextStream = stream;
     }
   }
   return nextStream;
+};
+
+export const isCurrentStream = (
+  stream:
+    | { data: { scheduled_at: Date | number } }
+    | { scheduled_at: Date | number }
+) => {
+  const streamDate = new Date(
+    "data" in stream ? stream.data.scheduled_at : stream.scheduled_at
+  );
+  console.log(new Date(Date.now() + 3 * HOUR_MS));
+  return (
+    new Date(Date.now()) > streamDate &&
+    new Date(Date.now()) < new Date(streamDate.getTime() + 3 * HOUR_MS)
+  );
 };
