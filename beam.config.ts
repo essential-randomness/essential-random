de1e30eb5db061c17cb58874714e00498598a2aa
Essential Randomness <essentialrandomn3ss@gmail.com>
2025-03-16 11:48:15 -0700

    add beam config

---
+++ beam.config.ts
@@ -0,0 +1,21 @@
+export default {
+  contentPaths: [
+    {
+      root: "./src/content/posts/",
+      glob: "**/_socials/",
+    },
+  ],
+  users: [
+    {
+      shorthand: "msboba",
+      name: "Ms Boba",
+      socials: {
+        mastodon: "essentialrandom@indiepocalypse.social",
+        bsky: "essentialrandom.bsky.social",
+        tumblr: "essential-randomness",
+        twitter: "EssentialRandom",
+        web: "https://www.essentialrandomness.com/",
+      },
+    },
+  ],
+};
