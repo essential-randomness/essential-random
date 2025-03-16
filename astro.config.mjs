7e6006aedb1b2d6b826dd2abf89ff22982f829ef
Essential Randomness <essentialrandomn3ss@gmail.com>
2025-03-16 12:06:40 -0700

    some static outputs

--- astro.config.mjs
+++ astro.config.mjs
@@ -81,7 +81,7 @@ export default defineConfig({
     //   status: 307,
     // },
   },
-  output: "static",
+  output: "server",
   adapter: vercel(),
   site: "https://www.essentialrandomness.com/",
 });
